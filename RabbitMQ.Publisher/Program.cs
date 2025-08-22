17ea777418d40956a5ad4f28a457e1e445beffe0
Safa Yüce <safayuce34@gmail.com>
2025-08-22 14:51:02 +0300

    rabbitmq
    rabbitmq

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Shared;
+using System;
+using System.Text;
+using System.Text.Json;
+namespace RabbitMQ.Publisher
+{
+    public enum LogNames
+    {
+        Critical=1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.Uri =  new Uri("amqps://imoearbm:SKLx7G1hs1_fNyGVw3MMDlc6bNLihq8x@woodpecker.rmq.cloudamqp.com/imoearbm");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            // channel.QueueDeclare("hello-queue", true, false, false); routing key is not used in this case (fanout)
+
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            
+
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers; // Set the headers property
+            properties.Persistent = true; // Make the message persistent
+
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Sample Product",
+                Price = 19.99m,
+                Stock = 100,    
+            };
+
+            var productJsonString = JsonSerializer.Serialize(product);
+
+
+            channel.BasicPublish("header-exchange", string.Empty, properties,Encoding.UTF8.GetBytes(productJsonString));
+
+            Console.WriteLine("Message sent to header-exchange with headers.");
+
+           
+        }
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Shared;
+using System;
+using System.Text.Json;
+namespace RabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://imoearbm:SKLx7G1hs1_fNyGVw3MMDlc6bNLihq8x@woodpecker.rmq.cloudamqp.com/imoearbm");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            var randomQueueName = channel.QueueDeclare().QueueName; // Create a temporary queue
+
+            channel.QueueBind(randomQueueName, "logs-fanout", "",null); // Bind the temporary queue to the fanout exchange
+
+            
+            
+            channel.BasicQos(0, 1, false); // Fair dispatch - only one message at a time per consumer
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName; // Create a new queue for the consumer
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match", "any"); // Use "all" for matching all headers
+
+            channel.QueueBind(queueName, "header-exchange",String.Empty,headers);
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Waiting for logs... Press Enter to exit.");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = System.Text.Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Product product = JsonSerializer.Deserialize<Product>(message);
+
+                Console.WriteLine($"Received Message =  { product.Id}- {product.Name}- {product.Price}-{product.Stock}");
+
+               // File.AppendAllText("logs.txt", $"{DateTime.Now}: {message}\n");
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
